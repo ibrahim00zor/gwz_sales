377d237cb22451e5bfb122df2c73743430e54417
ibrahim00zor <240735244@ogrenci.karatay.edu.tr>
2026-07-23 01:44:00 +0300

    new

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,6 +1,6 @@
 SELECT
 SAFE_CAST(date_date AS DATE) AS date_date,
-SUM(purchase_cost) AS daily_purchase_cost,
-SUM(TURNOVER) AS daily_turnover
+ROUND(SUM(purchase_cost), 2) AS daily_purchase_cost,
+ROUND(SUM(TURNOVER), 2) AS daily_turnover
 FROM course14.gwz_sales
 GROUP BY date_date
